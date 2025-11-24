f7f45365970362ac8387c9e6713deb3c86d6db24
4drea4 <bakerand@outlook.com>
2025-11-24 12:38:41 -0600

    I got map to load

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//function to bring in api ip data
 async function getAPI(api){
   const apiKey = "at_Dw02MjcDxw7tH0ttIuN7b1nlHrV1J";
   const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${api}`;
@@ -13,3 +14,14 @@ async function getAPI(api){
 }
 
 getAPI("8.8.8.8").then(data => console.log(data));
+
+//event listener on button
+
+
+
+//map
+const map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+
+}).addTo(map);
